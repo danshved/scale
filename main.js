30ddd9cf9103851c62bc22fa1a949f0b3de67acf
Dan Shved <danshved@gmail.com>
2020-04-05 18:52:56 +0200

    Use an OscillatorNode instead of a buffer

--- main.js
+++ main.js
@@ -1,35 +1,23 @@
-// Initialize audio context.
 var AudioContext = window.AudioContext || window.webkitAudioContext;
 var context = new AudioContext();
+var oscillator = null;
 
-// Prepare the array with samples.
-var arr = [], volume = 0.2, seconds = 0.5, tone = 441;
-for (var i = 0; i < context.sampleRate * seconds; i++) {
-    arr[i] = sineWaveAt(i, tone) * volume
+function play() {
+    console.log("Clicked Play");
+    if (oscillator === null) {
+        oscillator = context.createOscillator();
+        oscillator.frequency = 441;
+        oscillator.connect(context.destination);
+        oscillator.start();
+    }
 }
 
-function playSound(arr) {
-    // Make a "buffer" object
-    var buf = new Float32Array(arr.length)
-    for (var i = 0; i < arr.length; i++)
-        buf[i] = arr[i]
-    var buffer = context.createBuffer(1, buf.length, context.sampleRate)
-    buffer.copyToChannel(buf, 0)
-
-    // Play the buffer
-    var source = context.createBufferSource();
-    source.buffer = buffer;
-    source.connect(context.destination);
-    source.start(0);
+function stop() {
+    console.log("Clicked Stop");
+    if (oscillator !== null) {
+        oscillator.stop();
+        oscillator = null;
+    }
 }
 
-function sineWaveAt(sampleNumber, tone) {
-    var sampleFreq = context.sampleRate / tone
-    return Math.sin(sampleNumber / (sampleFreq / (Math.PI*2)))
-}
-
-
-function doClick() {
-    console.log("Clicked");
-    playSound(arr);
-}
+// TODO: add gain note (right now it is too loud).
